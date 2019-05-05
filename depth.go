5f1eed6da687dd6217882d275f7c03332673997d
Drumato <drumatech109@gmail.com>
2019-05-06 00:00:12 +0900

    pretty print

--- depth.go
+++ depth.go
@@ -41,8 +41,10 @@ func init() {
 	app.Author = AUTHOR
 	app.Email = LINK
 	app.Flags = []cli.Flag{
+		cli.BoolFlag{Name: "dump-source", Usage: "show code like UNIX cat command"},
 		cli.BoolFlag{Name: "dump-tokens", Usage: "dump tokens by lexer"},
 		cli.BoolFlag{Name: "dump-ast", Usage: "dump ast by recursive-descent parser"},
+		cli.BoolFlag{Name: "dump-hex", Usage: "dump binary by hex"},
 		cli.BoolFlag{Name: "dump-ir", Usage: "dump ir"},
 		cli.BoolFlag{Name: "print-stdout", Usage: "print stdout the result of the processing"},
 		cli.BoolFlag{Name: "-until-compile", Usage: "stop processing when succeed compile"},
@@ -74,10 +76,15 @@ func Start(c *cli.Context) error {
 		if err != nil {
 			return err
 		}
+		if c.Bool("dump-source") {
+			fmt.Printf("%s\n", aurora.Bold(aurora.Blue("----------------input source----------------")))
+			fmt.Println(string(b))
+		}
 		input = string(b)
 		lexer = lex.New(input, sourcecode)
 	}
 	if c.Bool("dump-tokens") {
+		fmt.Printf("%s\n", aurora.Bold(aurora.Blue("----------------dump tokens----------------")))
 		tok := lexer.NextToken()
 		for tok.Type != token.EOF {
 			fmt.Printf("%+v\n", tok)
@@ -94,6 +101,7 @@ func Start(c *cli.Context) error {
 	parse.AllocateRegisters(manager)
 	//fmt.Println(aurora.Bold(aurora.Blue("now compiling...")))
 	if c.Bool("dump-ir") {
+		fmt.Printf("%s\n", aurora.Bold(aurora.Blue("----------------dump IRs----------------")))
 		for fn, irs := range manager.FuncTable {
 			fmt.Printf("%s IR:\n", aurora.Bold(aurora.Blue(fn.Name)))
 			for _, ir := range irs {
@@ -110,6 +118,7 @@ func Start(c *cli.Context) error {
 		}
 
 		if c.Bool("print-stdout") {
+			fmt.Printf("%s\n", aurora.Bold(aurora.Blue("----------------assembly----------------")))
 			codegen.Gen(manager, os.Stdout, lexer.Filename)
 		} else {
 			codegen.Gen(manager, f, lexer.Filename)
@@ -128,12 +137,15 @@ func Start(c *cli.Context) error {
 	}
 	asms := asm.Parse(string(binaries))
 	asm.Semantic(os.Stdout, asms)
-	for _, as := range asms {
-		if as.Op.Code == 0 {
-			fmt.Printf("%s: % x\n", as.Op.Name, as.Op.Code)
-			continue
+	if c.Bool("dump-hex") {
+		fmt.Printf("%s\n", aurora.Bold(aurora.Blue("----------------hexdump----------------")))
+		for _, as := range asms {
+			if as.Op.Code == 0 {
+				fmt.Printf("%s: % x\n", as.Op.Name, as.Op.Code)
+				continue
+			}
+			fmt.Printf("% x\n", as.Op.Code)
 		}
-		fmt.Printf("% x\n", as.Op.Code)
 	}
 	if c.Bool("until-assemble") {
 		return nil
