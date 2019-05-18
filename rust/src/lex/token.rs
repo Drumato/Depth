bd500ccd829c72bd83c5ce7e630b1d74b28131df
Drumato <drumatech109@gmail.com>
2019-05-18 22:51:31 +0900

    :contruction: failed to load library from lex

---
+++ rust/src/lex/token.rs
@@ -0,0 +1,163 @@
+enum TokenType {
+    /* for identifying Type */
+    TkIllegal,
+    TkIdent,
+    TkEof,
+    TkFunction,
+    TkCharlit,
+    TkIntlit,
+    TkStrlit,
+    TkReallit,
+
+    /* keywords */
+    TkMutable, //mut
+    TkF,       //f
+    TkTrue,    //true
+    TkFalse,   //false
+    TkLoop,    //loop
+    TkFor,     //for
+    TkLet,     //let
+    TkConst,   //const
+    TkIf,      //if
+    TkElse,    //else
+    TkReturn,  //return
+    TkStruct,  //struct
+
+    /* types */
+    TkBool,   //bool
+    TkChar,   //ch
+    TkString, //str
+    TkU8,     //u8
+    TkU16,    //u16
+    TkU32,    //u32
+    TkU64,    //u64
+    TkU128,   //u128
+    TkI8,     //i8
+    TkI16,    //i16
+    TkI32,    //i32
+    TkI64,    //i64
+    TkI128,   //i128
+    TkF32,    //f32
+    TkF64,    //f64
+
+    /* marks */
+    TkAssign,
+    TkPlus,
+    TkIncre,
+    TkMinus,
+    TkDecre,
+    TkBang,
+    TkPipe,
+    TkAmpersand,
+    TkSlash,
+    TkStar,
+    TkPercent,
+    TkPlusassign,
+    TkMinusassign,
+    TkStarassign,
+    TkSlashassign,
+    TkPercentassign,
+    TkArrow,
+    TkBackslash,
+
+    TkLt,
+    TkGt,
+    TkEq,
+    TkNoteq,
+    TkLteq,
+    TkGteq,
+    TkLogor,
+    TkLogand,
+    TkComma,
+    TkSemicolon,
+    TkColon,
+    TkDot,
+    TkLparen,
+    TkRparen,
+    TkLbrace,
+    TkRbrace,
+    TkLbracket,
+    TkRbracket,
+}
+
+impl TokenType {
+    pub fn string(&self) -> &str {
+        match self {
+            TokenType::TkIllegal => "ILLEGAL",
+            TokenType::TkIdent => "IDENTIFER",
+            TokenType::TkEof => "EOF",
+            TokenType::TkFunction => "FUNCTION",
+            TokenType::TkIntlit => "INT-LITERAL",
+            TokenType::TkCharlit => "CHAR-LITERAL",
+            TokenType::TkStrlit => "STRING-LITERAL",
+            TokenType::TkReallit => "REAL-LITERAL",
+            TokenType::TkMutable => "mut",
+            TokenType::TkF => "f",
+            TokenType::TkTrue => "true",
+            TokenType::TkFalse => "false",
+            TokenType::TkLoop => "loop",
+            TokenType::TkFor => "for",
+            TokenType::TkLet => "let",
+            TokenType::TkConst => "const",
+            TokenType::TkIf => "if",
+            TokenType::TkElse => "else",     //else
+            TokenType::TkReturn => "return", //return
+            TokenType::TkStruct => "struct", //struct
+
+            /* types */
+            TokenType::TkBool => "bool",  //bool
+            TokenType::TkChar => "ch",    //ch
+            TokenType::TkString => "str", //str
+            TokenType::TkU8 => "u8",      //u8
+            TokenType::TkU16 => "u16",    //u16
+            TokenType::TkU32 => "u32",    //u32
+            TokenType::TkU64 => "u64",    //u64
+            TokenType::TkU128 => "u128",
+            TokenType::TkI8 => "i8",     //i16
+            TokenType::TkI16 => "i16",   //i16
+            TokenType::TkI32 => "i32",   //i32
+            TokenType::TkI64 => "i64",   //i64
+            TokenType::TkI128 => "i128", //i128
+            TokenType::TkF32 => "f32",   //f32
+            TokenType::TkF64 => "f64",   //f64
+
+            /* marks */
+            TokenType::TkAssign => "=",
+            TokenType::TkEq => "==",
+            TokenType::TkPlus => "+",
+            TokenType::TkIncre => "++",
+            TokenType::TkPlusassign => "+=",
+            TokenType::TkMinus => "-",
+            TokenType::TkDecre => "--",
+            TokenType::TkMinusassign => "-=",
+            TokenType::TkBang => "!",
+            TokenType::TkNoteq => "!=",
+            TokenType::TkPipe => "|",
+            TokenType::TkLogor => "||",
+            TokenType::TkAmpersand => "&",
+            TokenType::TkLogand => "&&",
+            TokenType::TkStar => "*",
+            TokenType::TkStarassign => "*=",
+            TokenType::TkSlash => "/",
+            TokenType::TkSlashassign => "/=",
+            TokenType::TkPercent => "%",
+            TokenType::TkPercentassign => "%=",
+            TokenType::TkArrow => "->",
+            TokenType::TkLt => "<",
+            TokenType::TkLteq => "<=",
+            TokenType::TkGt => ">",
+            TokenType::TkGteq => ">=",
+            TokenType::TkComma => ",",
+            TokenType::TkSemicolon => ";",
+            TokenType::TkColon => ":",
+            TokenType::TkDot => ".",
+            TokenType::TkBackslash => "\\",
+            TokenType::TkLparen => "(",
+            TokenType::TkRparen => ")",
+            TokenType::TkLbrace => "{",
+            TokenType::TkRbrace => "}",
+            TokenType::TkLbracket => "[",
+            TokenType::TkRbracket => "]",
+        }
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,9 +4,13 @@ use std::io::prelude::*;
 use std::io::{BufReader, Write};
 use yaml_rust::{YamlEmitter, YamlLoader};
 
-extern crate asm;
+#[macro_use]
+extern crate clap;
+use clap::App;
 
 fn main() -> Result<(), Box<std::error::Error>> {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
     let cfg = &get_yaml()?[0];
     let mut out_str = String::new();
     {
@@ -14,54 +18,8 @@ fn main() -> Result<(), Box<std::error::Error>> {
         emitter.dump(cfg).unwrap(); // dump the YAML object to a String
     }
     println!("{}", out_str);
-    /*let magicnumber: u128 = 0x7f454c46020101000000000000000000; //0~15 byte
-    let fty: u16 = 0x1; //16~17
-    let march: u16 = 0x3e; //18~19
-    let fver: u32 = 0x1; //20~23
-    let epoint: u64 = 0x0; // 24~31
-    let phoff: u32 = 0x00; //
-    let shoff: u32 = 0x82;
-    let flags: u32 = 0x0;
-    let size: u16 = 0x40;
-    let phsize: u16 = 0x00;
-    let phnum: u16 = 0x0;
-    let shsize: u16 = 0x40;
-    let shnum: u16 = 0xb;
-    let shstridx: u16 = 0xa;
-    let param = (
-        magicnumber,
-        fty,
-        march,
-        fver,
-        epoint,
-        phoff,
-        shoff,
-        flags,
-        size,
-        phsize,
-        phnum,
-        shsize,
-        shnum,
-        shstridx,
-    );
-    let ehdr = asm::elf::Elf64Header::new(param);
-    let mut file = File::create("sample.o")?;
-    write!(file, "{}", ehdr.binary_dump(false))?;
-    file.flush()?;*/
+    println!("{}", lex::TkIllegal.string());
     Ok(())
-    //lexing();
-}
-
-fn lexing() {
-    let mut content: String = String::new();
-    match File::open("tmp.s") {
-        Ok(file) => {
-            let mut buf_file = BufReader::new(file);
-            asm::lex::iter_lines(&mut buf_file);
-        }
-        Err(e) => println!("Error found in tmp.s: {}", e),
-    }
-    println!("{}", content);
 }
 
 fn get_yaml() -> Result<Vec<yaml_rust::Yaml>, yaml_rust::ScanError> {
