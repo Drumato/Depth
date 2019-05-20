032d4a7359f7ba42868e36e97065b0143dc36a1f
Drumato <drumatech109@gmail.com>
2019-05-20 11:48:09 +0900

    :construction:

--- rust/src/binary/mod.rs
+++ rust/src/binary/mod.rs
@@ -1,1 +1,11 @@
 pub mod bytes;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_range() {
+        let mut b: super::bytes::Binaryen =
+            super::bytes::Binaryen::new((vec![0x7f, 0x45, 0x4c, 0x46], false));
+        assert_eq!(b.range(1, 2).unwrap(), vec![0x45, 0x4c]);
+    }
+}
