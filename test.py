7c234f877a93e44b6f3a94f8ba4c30eb9e698d65
Drumato <drumatech109@gmail.com>
2019-09-10 02:21:18 -0700

    [feat]

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ class Color:
 
 
 def make():
-    test_files = sorted(os.listdir("./test"))
-    f = open("test/expect.txt", "w")
+    test_files = sorted(os.listdir("./test/testc/"))
+    f = open("test/testc/expect.txt", "w")
     for idx, test_file in enumerate(test_files):
         if test_file == "expect.txt":
             continue
@@ -20,8 +20,9 @@ def make():
     f.close()
 
 
-def test():
-    f = open("test/expect.txt", "r")
+def test_compile():
+    print(f"{Color.GREEN}++++++++++++++++test-compile++++++++++++++++{Color.CLEAR}")
+    f = open("test/testc/expect.txt", "r")
     content = f.read()
     cases = {}
     cases = {
@@ -30,23 +31,50 @@ def test():
         if len(line) > 0
     }
     for filename, expect in cases.items():
+        fn = f"test/testc/{filename}"
+        f = open(fn)
         p = subprocess.Popen(
-            f"./target/debug/depth test/{filename} --intel -S > c.s; gcc c.s ; ./a.out",
-            shell=True,
+            f"./target/debug/depth {fn} --intel -S > c.s; gcc c.s ; ./a.out", shell=True
         )
         exit_status = p.wait()
         if exit_status != expect:
             print(
-                f'[{filename}]{open(f"test/{filename}").read()} => {Color.RED}{expect} expected but got {exit_status}{Color.CLEAR}'
+                f"[{filename}]{f.read()} => {Color.RED}{expect} expected but got {exit_status}{Color.CLEAR}"
             )
             sys.exit(1)
         else:
+            print(f"[{filename}]{f.read()} => {Color.BLUE}{expect}{Color.CLEAR}")
+    print(f"{Color.GREEN}All Test Passed.{Color.CLEAR}")
+
+
+def test_assemble():
+    print(f"{Color.GREEN}++++++++++++++++test-assemble++++++++++++++++{Color.CLEAR}")
+    f = open("test/testa/expect.txt", "r")
+    content = f.read()
+    cases = {}
+    cases = {
+        line.split()[0]: int(line.split()[1])
+        for line in content.split("\n")
+        if len(line) > 0
+    }
+    for filename, expect in cases.items():
+        fn = f"test/testa/{filename}"
+        f = open(fn)
+        p = subprocess.Popen(
+            f"./target/debug/depth {fn} ; gcc c.o ; ./a.out", shell=True
+        )
+        exit_status = p.wait()
+        if exit_status != expect:
             print(
-                f'[{filename}]{open(f"test/{filename}").read()} => {Color.BLUE}{expect}{Color.CLEAR}'
+                f"[{filename}]{f.read()} => {Color.RED}{expect} expected but got {exit_status}{Color.CLEAR}"
             )
+            sys.exit(1)
+        else:
+            print(f"[{filename}]{f.read()} => {Color.BLUE}{expect}{Color.CLEAR}")
     print(f"{Color.GREEN}All Test Passed.{Color.CLEAR}")
 
 
 if __name__ == "__main__":
-    test()
+    test_compile()
+    test_assemble()
     # make()
