4b19a2d8e0769e9f675c8535e82a17adf3726de5
Drumato <drumatech109@gmail.com>
2019-09-29 04:55:04 -0700

    [feat-compile] build inference-graph using matrix

--- src/compile/backend/regalloc.rs
+++ src/compile/backend/regalloc.rs
@@ -7,7 +7,11 @@ static AVAILABLE_REG: usize = 9; // rax, rdx, rcx, rdi, rsi, r8, r9, r10, r11
 impl Optimizer {
     pub fn regalloc(&mut self) {
         let mut reg_maps: Vec<HashSet<Operand>> = vec![HashSet::new(); self.tacs.len()];
-        let mut graph: BTreeMap<Operand, Vec<Operand>> = BTreeMap::new();
+        let mut graph: Vec<Vec<bool>> = vec![vec![false; self.living.len()]; self.living.len()];
+        let mut vars: BTreeMap<Operand, usize> = BTreeMap::new();
+        for (idx, (op, _range)) in self.living.iter().enumerate() {
+            vars.insert(op.clone(), idx);
+        }
         for (idx, t) in self.tacs.iter_mut().enumerate() {
             for (op, range) in self.living.iter() {
                 if range.0 <= idx && idx <= range.1 {
@@ -15,16 +19,11 @@ impl Optimizer {
                 }
             }
             for op in reg_maps[idx].iter() {
-                graph.insert(op.clone(), Vec::new());
-            }
-            for op in reg_maps[idx].iter() {
                 for op2 in reg_maps[idx].iter() {
                     if op == op2 {
                         continue;
                     }
-                    if let Some(edges) = graph.get_mut(op) {
-                        edges.push(op2.clone());
-                    }
+                    graph[*vars.get(op).unwrap()][*vars.get(op2).unwrap()] = true;
                 }
             }
         }
