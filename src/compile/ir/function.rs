99df3277ada455f79367d5e0eca0d6e0e601dfd8
Drumato <drumatech109@gmail.com>
2019-11-20 02:48:46 -0800

    [refactor] using function

--- src/compile/ir/function.rs
+++ src/compile/ir/function.rs
@@ -1,11 +1,20 @@
+use super::super::super::ce::types::Error;
+use super::super::frontend::frontmanager::frontmanager::Symbol;
+use super::super::frontend::parse::node::{Func, Node};
+use super::super::frontend::sema::semantics::Type;
 use super::basicblock::BasicBlock;
 use super::instruction::Instruction;
+use super::instruction::Instruction as Inst;
+use super::llvm_type::LLVMType;
+use super::llvm_value::{LLVMSymbol, LLVMValue};
+use std::collections::BTreeMap;
 pub struct Function {
     pub blocks: Vec<BasicBlock>,
     // ty: FuncType
     pub name: String,
     pub insert_point: usize,
     pub label: usize,
+    pub env: BTreeMap<String, LLVMSymbol>,
 }
 
 impl Function {
@@ -16,6 +25,7 @@ impl Function {
             name: name,
             insert_point: 0,
             label: 1,
+            env: BTreeMap::new(),
         }
     }
     pub fn dump(&self) {
@@ -36,5 +46,69 @@ impl Function {
             _ => (),
         }
         self.blocks[self.insert_point].insts.push(inst);
+    }
+    pub fn build_function(&mut self, f: &Func) {
+        for st in f.stmts.iter() {
+            match st {
+                Node::RETURN(bexpr) => self.build_return(*bexpr.clone()),
+                Node::LET(ident_name, bexpr) => {
+                    if let Some(v) = f.env.sym_table.get(ident_name) {
+                        self.build_let(ident_name.to_string(), v, *bexpr.clone())
+                    } else {
+                        Error::LLVM.found(&format!("{} is not defined", &ident_name));
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+    fn build_let(&mut self, ident_name: String, symbol: &Symbol, expr: Node) {
+        if let Ok(ty) = &symbol.ty {
+            let llvm_type = self.get_llvmtype_from_type(ty.clone());
+            let alignment = llvm_type.alignment();
+            let label = self.label;
+            let llvm_symbol = LLVMSymbol::new(label, llvm_type.clone());
+            self.env.insert(ident_name, llvm_symbol);
+            self.add_inst(Inst::Alloca(label, llvm_type.clone(), alignment));
+            match expr {
+                Node::INTEGER(value) => {
+                    let llvm_value = LLVMValue::INTEGER(value);
+                    self.add_inst(Inst::Store(llvm_type, llvm_value, label, alignment));
+                }
+                Node::IDENT(name) => {
+                    if let Some(llvm_symbol) = self.env.get(&name) {
+                        let llvm_value = LLVMValue::VREG(llvm_symbol.label);
+                        self.add_inst(Inst::Store(llvm_type, llvm_value, label, alignment));
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+    fn get_llvmtype_from_type(&mut self, ty: Type) -> LLVMType {
+        match ty {
+            Type::INTEGER => LLVMType::I64,
+            _ => LLVMType::UNKNOWN,
+        }
+    }
+    fn build_return(&mut self, expr: Node) {
+        match expr {
+            Node::INTEGER(value) => {
+                let llvm_value = LLVMValue::INTEGER(value);
+                self.add_inst(Inst::RetTy(LLVMType::I64, llvm_value));
+            }
+            Node::IDENT(name) => {
+                if let Some(llvm_symbol) = self.env.get(&name) {
+                    let llvm_type = llvm_symbol.ty.clone();
+                    let alignment = llvm_type.alignment();
+                    let llvm_value = LLVMValue::VREG(llvm_symbol.label);
+                    let label = self.label;
+                    self.add_inst(Inst::Load(label, llvm_type.clone(), llvm_value, alignment));
+                    let dst_value = LLVMValue::VREG(label);
+                    self.add_inst(Inst::RetTy(llvm_type, dst_value));
+                }
+            }
+            _ => (),
+        }
     }
 }
--- src/compile/ir/llvm.rs
+++ src/compile/ir/llvm.rs
@@ -1,19 +1,12 @@
-use super::super::super::ce::types::Error;
-use super::super::frontend::frontmanager::frontmanager::{FrontManager, Symbol};
-use super::super::frontend::parse::node::{Func, Node};
-use super::super::frontend::sema::semantics::Type;
+use super::super::frontend::frontmanager::frontmanager::FrontManager;
+use super::super::frontend::parse::node::Func;
 use super::context::Context;
 use super::function::Function as LLVMFunc;
-use super::instruction::Instruction as Inst;
-use super::llvm_type::LLVMType;
-use super::llvm_value::{LLVMSymbol, LLVMValue};
 use super::module::Module;
 
-use std::collections::BTreeMap;
 pub struct IRBuilder {
     module: Module,
     ctx: Context,
-    env: BTreeMap<String, LLVMSymbol>,
     functions: Vec<Func>,
 }
 impl IRBuilder {
@@ -25,80 +18,9 @@ impl IRBuilder {
     fn build_module(&mut self) {
         let functions = self.functions.clone();
         for f in functions.iter() {
-            let llvm_func = self.build_function(f);
+            let mut llvm_func = LLVMFunc::new(f.name.to_string());
+            llvm_func.build_function(f);
             self.module.add_func(llvm_func);
-        }
-    }
-    fn build_function(&mut self, f: &Func) -> LLVMFunc {
-        let mut llvm_func = LLVMFunc::new(f.name.to_string());
-        for st in f.stmts.iter() {
-            match st {
-                Node::RETURN(bexpr) => self.build_return(&mut llvm_func, *bexpr.clone()),
-                Node::LET(ident_name, bexpr) => {
-                    if let Some(v) = f.env.sym_table.get(ident_name) {
-                        self.build_let(&mut llvm_func, ident_name.to_string(), v, *bexpr.clone())
-                    } else {
-                        Error::LLVM.found(&format!("{} is not defined", &ident_name));
-                    }
-                }
-                _ => (),
-            }
-        }
-        llvm_func
-    }
-    fn build_return(&mut self, llvm_func: &mut LLVMFunc, expr: Node) {
-        match expr {
-            Node::INTEGER(value) => {
-                let llvm_value = LLVMValue::INTEGER(value);
-                llvm_func.add_inst(Inst::RetTy(LLVMType::I64, llvm_value));
-            }
-            Node::IDENT(name) => {
-                if let Some(llvm_symbol) = self.env.get(&name) {
-                    let llvm_type = llvm_symbol.ty.clone();
-                    let alignment = llvm_type.alignment();
-                    let llvm_value = LLVMValue::VREG(llvm_symbol.label);
-                    let label = llvm_func.label;
-                    llvm_func.add_inst(Inst::Load(label, llvm_type.clone(), llvm_value, alignment));
-                    let dst_value = LLVMValue::VREG(label);
-                    llvm_func.add_inst(Inst::RetTy(llvm_type, dst_value));
-                }
-            }
-            _ => (),
-        }
-    }
-    fn build_let(
-        &mut self,
-        llvm_func: &mut LLVMFunc,
-        ident_name: String,
-        symbol: &Symbol,
-        expr: Node,
-    ) {
-        if let Ok(ty) = &symbol.ty {
-            let llvm_type = self.get_llvmtype_from_type(ty.clone());
-            let alignment = llvm_type.alignment();
-            let label = llvm_func.label;
-            let llvm_symbol = LLVMSymbol::new(label, llvm_type.clone());
-            self.env.insert(ident_name, llvm_symbol);
-            llvm_func.add_inst(Inst::Alloca(label, llvm_type.clone(), alignment));
-            match expr {
-                Node::INTEGER(value) => {
-                    let llvm_value = LLVMValue::INTEGER(value);
-                    llvm_func.add_inst(Inst::Store(llvm_type, llvm_value, label, alignment));
-                }
-                Node::IDENT(name) => {
-                    if let Some(llvm_symbol) = self.env.get(&name) {
-                        let llvm_value = LLVMValue::VREG(llvm_symbol.label);
-                        llvm_func.add_inst(Inst::Store(llvm_type, llvm_value, label, alignment));
-                    }
-                }
-                _ => (),
-            }
-        }
-    }
-    fn get_llvmtype_from_type(&mut self, ty: Type) -> LLVMType {
-        match ty {
-            Type::INTEGER => LLVMType::I64,
-            _ => LLVMType::UNKNOWN,
         }
     }
     fn new(module_id: String, funcs: Vec<Func>) -> Self {
@@ -108,7 +30,6 @@ impl IRBuilder {
             module: module,
             ctx: ctx,
             functions: funcs,
-            env: BTreeMap::new(),
         }
     }
 }
