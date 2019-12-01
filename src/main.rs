3d50e1533db054ce7c05c3ce00cd5034221ebaee
Drumato <drumatech109@gmail.com>
2019-11-30 20:22:27 -0800

    [refactor]

--- src/main.rs
+++ src/main.rs
@@ -26,24 +26,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
+    /* compile phase */
     let file_name = matches.value_of("source").unwrap();
     let assembly: String = compile(file_name.to_string(), &matches)
         + "_start:\n  call main\n  mov rdi, rax\n  mov rax, 60\n  syscall\n";
+
+    /* if stop-c given so output the assembly-code to file. */
     if matches.is_present("stop-c") {
         let output_path: String = file_name.split(".").collect::<Vec<&str>>()[0].to_string() + ".s";
         let mut file = File::create(output_path)?;
         file.write_all(assembly.as_bytes())?;
         std::process::exit(0);
     }
+
+    /* assembly phase */
     let elf_binary: ELF = if !file_name.contains(".o") {
         assemble(assembly.to_string(), &matches)
     } else {
+        // read the object file then construct ELF struct.
         ELF::read_elf(&file_name.to_string())
     };
+
+    /* if stop-a given so output the object-file. */
     if matches.is_present("stop-a") {
         let output_path = file_name.split(".").collect::<Vec<&str>>()[0].to_string() + ".o";
         output_file_with_binary(output_path, elf_binary);
     } else {
+        // link the object-file then we generate ET_EXEC from it.
         let exec_file: ELF = link::linker::Linker::linking(vec![elf_binary]);
         output_file_with_binary("a.out".to_string(), exec_file);
     }
@@ -53,35 +62,58 @@ fn compile(file_name: String, matches: &clap::ArgMatches) -> String {
     if !file_name.contains(".dep") {
         return read_file(&file_name);
     }
+    /* tokenize */
     let tokens: Vec<f::token::token::Token> = lex_phase(file_name.to_string(), &matches);
+
+    /* parse */
     let funcs: Vec<f::parse::node::Func> = parse_phase(&matches, tokens);
     let mut front_manager: fm::FrontManager = fm::FrontManager::new(funcs);
+
+    /* semantic-analyze */
     front_manager.semantics();
+
     if matches.is_present("dump-symbol") {
         front_manager.dump_symbol();
     }
+
+    /* constant-fold with ast */
     front_manager.constant_folding();
+
+    /* emit-llvm path */
     if matches.is_present("emit-llvm") {
         llvm::emit_llvm(file_name, front_manager);
         std::process::exit(0);
     }
+
+    /* generate three-address-code from ast */
     front_manager.gen_tacs();
     let tacs: Vec<Tac> = front_manager.tacs;
+
+    /* backend */
     let mut optimizer: b::Optimizer = b::Optimizer::new(tacs);
+
+    /* build the control-flow-graph */
     optimizer.build_cfg();
     if matches.is_present("dump-cfg") {
         optimizer.dump_cfg();
     }
+
+    /* TODO: not implemented yet */
     if matches.is_present("Opt1") {
         optimizer.build_cfg_for_reaching();
         optimizer.reaching_definition();
         optimizer.available_expression();
     }
+
+    /* append the information for liveness */
     optimizer.build_cfg_for_liveness();
+
+    /* liveness-analysis */
     optimizer.liveness();
     if matches.is_present("dump-liveness") {
         optimizer.dump_liveness();
     }
+    /* linear register allocation */
     optimizer.regalloc();
     if matches.is_present("dump-tac") {
         eprintln!("{}", "--------dump-tac--------".blue().bold());
@@ -89,33 +121,36 @@ fn compile(file_name: String, matches: &clap::ArgMatches) -> String {
             eprintln!("{}: {}", i, tac.string());
         }
     }
-    let mut assembler_code: String = b::codegen::genx64(optimizer.tacs);
-    if matches.is_present("intel") {
-        assembler_code = format!(".intel_syntax noprefix\n.global main\n{}", assembler_code);
-    }
-    assembler_code
+
+    /* codegen */
+    b::codegen::genx64(optimizer.tacs)
 }
+
 fn assemble(assembler_code: String, matches: &clap::ArgMatches) -> ELF {
     use object::elf::elf64;
+
+    /* tokenize */
     let tokens: Vec<a::lex::Token> = a::lex::lexing(assembler_code);
+
+    /* parse */
     let (instructions, info_map, relas) = a::parse::parsing(tokens);
     if matches.is_present("dump-inst") {
         dump_inst(&instructions, &info_map);
     }
+
     let (code_map, mut relas) = a::gen::generate(instructions, info_map, relas);
-    let shstrtab: Vec<u8> = elf64::strtab(vec![
-        ".text",
-        ".symtab",
-        ".strtab",
-        ".rela.text",
-        ".shstrtab",
-    ]);
-    let strs: Vec<&str> = code_map
+
+    let shstrtab = build_shstrtab();
+
+    /* build symbol-names from map */
+    let symbol_names = code_map
         .iter()
         .map(|(name, _)| name.as_str())
         .collect::<Vec<&str>>();
-    let mut symbols: Vec<elf64::Symbol> = Vec::with_capacity(100);
-    symbols.push(elf64::init_nullsym());
+
+    /* initialize with null symbol. */
+    let mut symbols: Vec<elf64::Symbol> = vec![elf64::init_nullsym()];
+
     let mut total_len: u64 = 0;
     let mut total_code: Vec<u8> = Vec::with_capacity(2048);
     let mut name: u32 = 1;
@@ -140,7 +175,7 @@ fn assemble(assembler_code: String, matches: &clap::ArgMatches) -> ELF {
         }
     }
     let mut elf_file = ELF::init();
-    let strtab: Vec<u8> = elf64::strtab(strs);
+    let strtab: Vec<u8> = elf64::strtab(symbol_names);
     elf_file.add_section(vec![], elf64::init_nullhdr(), "null");
     elf_file.add_section(total_code, elf64::init_texthdr(total_len), ".text");
     let symbol_length = symbols.len();
@@ -262,4 +297,15 @@ fn output_file_with_binary(output_path: String, binary: ELF) {
         Ok(_) => (),
         Err(e) => eprintln!("{}", e),
     }
+}
+
+fn build_shstrtab() -> Vec<u8> {
+    use object::elf::elf64;
+    elf64::strtab(vec![
+        ".text",
+        ".symtab",
+        ".strtab",
+        ".rela.text",
+        ".shstrtab",
+    ])
 }
